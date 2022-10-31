d22a8528917ed6bb42e896afa7fac527e0f58e27
TogekissML <116655572+TogekissML@users.noreply.github.com>
2022-10-31 23:35:48 +0000

    Add files via upload

---
+++ room.js
@@ -0,0 +1,50 @@
+consoleref.log(
+    '%cby togekiss lol :P',
+    'background: lightblue; color: red; display: block; padding: 1px;',
+);
+setTimeout(() => {
+setTimeout(async () => {
+    await $.getScript("https://cdn.jsdelivr.net/gh/parseml/many-deobf@latest/deobf.js");
+    function getO73(){
+        window.parent = {};
+            for(i in window){
+            if(i[0] == "O"){
+            consoleref.log(i)
+        if(typeof window[i].prototype !== "undefined"){
+            if(typeof window[i].prototype.deleteThingAt !== "undefined"){
+            return window[i];
+            }
+        }}
+        }};
+        build = Deobfuscator.function(getO73().prototype, "map_pta", true);
+        for (let i = 38; i < 55; i++) {
+            getO73().prototype[build](a = {tid: '551b37524dbe33d1182d76de', rotation: 0, flip: 0}, b = {p: 0, a: ig.game.areaId, x: i, y: -14}, c = true, d = undefined);
+        };
+for (let i = -15; i > -22; i--) {
+            getO73().prototype[build](a = {tid: '551b37524dbe33d1182d76de', rotation: 0, flip: 0}, b = {p: 0, a: ig.game.areaId, x: 38, y: i}, c = true, d = undefined);
+        };
+        for (let i = -15; i > -22; i--) {
+            getO73().prototype[build](a = {tid: '551b37524dbe33d1182d76de', rotation: 0, flip: 0}, b = {p: 0, a: ig.game.areaId, x: 54, y: i}, c = true, d = undefined);
+        };
+        for (let i = 38; i < 55; i++) {
+            getO73().prototype[build](a = {tid: '551b37524dbe33d1182d76de', rotation: 0, flip: 0}, b = {p: 0, a: ig.game.areaId, x: i, y: -21}, c = true, d = undefined);
+        };
+        for (let i = 35; i < 38; i++) {
+            getO73().prototype[build](a = {tid: '531405b27f5e940576000404', rotation: 0, flip: 0}, b = {p: 0, a: ig.game.areaId, x: i+1, y: -16}, c = true, d = undefined); getO73().prototype[build](a = {tid: '531405b27f5e940576000404', rotation: 2, flip: 1}, b = {p: 0, a: ig.game.areaId, x: i+1, y: -15}, c = true, d = undefined);
+        };
+        for (let i = 35; i < 38; i++) {
+            getO73().prototype[build](a = {tid: '531405b27f5e940576000404', rotation: 0, flip: 0}, b = {p: 0, a: ig.game.areaId, x: i+1, y: -16}, c = true, d = undefined); getO73().prototype[build](a = {tid: '531405b27f5e940576000404', rotation: 2, flip: 1}, b = {p: 0, a: ig.game.areaId, x: i+1, y: -15}, c = true, d = undefined);
+        };
+        getO73().prototype[build](a = {tid: '6241efa3b1de041c195e286c', rotation: 0, flip: 0}, b = {p: 0, a: ig.game.areaId, x: 37, y: -16}, c = true, d = undefined); getO73().prototype[build](a = {tid: '6241efa3b1de041c195e286c', rotation: 2, flip: 1}, b = {p: 0, a: ig.game.areaId, x: 37, y: -15}, c = true, d = undefined);
+        for (let y = -20; y < -14; y++) {
+            for (let i = 39; i < 54 ; i++) {
+            getO73().prototype[build](a = {tid: '548de69e6e574c78310c7a19', rotation: 0, flip: 0}, b = {p: 0, a: ig.game.areaId, x: i, y: y}, c = true, d = undefined);
+            }
+        };
+        for (let i = -15; i > -21; i--) {
+            getO73().prototype[build](a = {tid: '56477c9ccb428e7a794f7467', rotation: 0, flip: 0}, b = {p: 0, a: ig.game.areaId, x: 52, y: i}, c = true, d = undefined);getO73().prototype[build](a = {tid: '56477c9ccb428e7a794f7467', rotation: 0, flip: 0}, b = {p: 0, a: ig.game.areaId, x: 40, y: i}, c = true, d = undefined);
+        };
+        getO73().prototype[build](a = {tid: '520f25356d8fe4323300002e', rotation: 0, flip: 0}, b = {p: 0, a: ig.game.areaId, x: 43, y: -19}, c = true, d = undefined); getO73().prototype[build](a = {tid: '520f20c1e06f57e47a000033', rotation: 0, flip: 0}, b = {p: 0, a: ig.game.areaId, x: 44, y: -19}, c = true, d = undefined);getO73().prototype[build](a = {tid: '520f2648d075cc134d000032', rotation: 0, flip: 0}, b = {p: 0, a: ig.game.areaId, x: 45, y: -19}, c = true, d = undefined); getO73().prototype[build](a = {tid: '520f250b6d8fe4323300002d', rotation: 0, flip: 0}, b = {p: 0, a: ig.game.areaId, x: 47, y: -19}, c = true, d = undefined);getO73().prototype[build](a = {tid: '520f23f6e06f57e47a000036', rotation: 0, flip: 0}, b = {p: 0, a: ig.game.areaId, x: 48, y: -19}, c = true, d = undefined); getO73().prototype[build](a = {tid: '520f23f6e06f57e47a000036', rotation: 0, flip: 0}, b = {p: 0, a: ig.game.areaId, x: 49, y: -19}, c = true, d = undefined);getO73().prototype[build](a = {tid: '520f23026d8fe4323300002c', rotation: 0, flip: 0}, b = {p: 0, a: ig.game.areaId, x: 50, y: -19}, c = true, d = undefined);
+    });
+
+    }, 0)
